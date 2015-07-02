53d18ca8e5b4eab5abe913fe3dc329ff32b27d9d
austin.lazarus@gmail.com <austin.lazarus@gmail.com>
2015-07-01 22:36:48 -0400

    commit - 01.07.15

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ app.run(debug = True)
 #app.run()
 #Bind to PORT if defined, otherwise default to 5000. 
 
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 4000))
 app.run(host='0.0.0.0', port=port)
